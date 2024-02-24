7252298a24f124faa0ce0fcd194d27240e4f0bb3
emorozov <zhenya.morozov@gmail.com>
2024-02-24 19:59:32 +0600

    testing classes

---
+++ tests/test_circle.py
@@ -0,0 +1,21 @@
+import pytest
+
+import source.shapes as shapes
+
+import math
+
+class TestCircle:
+
+    def setup_method(self, method):
+        print(f"Setting up {method}")
+        self.circle = shapes.Circle(10)
+
+    def teardown_method(self, method):
+        print(f"Tearing down {method}")
+        del self.circle
+
+    def test_area(self):
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
+
+    def test_two(self):
+        assert True
