3e9f6b2af18e0043250af411740aebd1dc2de8cf
emorozov <zhenya.morozov@gmail.com>
2024-02-24 20:30:57 +0600

    more fixtures

--- tests/test_rectancle.py
+++ tests/test_rectancle.py
@@ -6,6 +6,10 @@ import source.shapes as shapes
 def my_rectangle():
     return shapes.Rectangle(10, 20)
 
+@pytest.fixture
+def weird_rectangle():
+    return shapes.Rectangle(5, 6)
+
 def test_area(my_rectangle):
     # my_rectangle = shapes.Rectangle(10, 20)
     assert my_rectangle.area() == 10 * 20
@@ -13,3 +17,6 @@ def test_area(my_rectangle):
 def test_perimeter(my_rectangle):
     # my_rectangle = shapes.Rectangle(10, 20)
     assert my_rectangle.perimeter() == 2 * (10 + 20)
+
+def test_not_equal(my_rectangle, weird_rectangle):
+    assert my_rectangle != weird_rectangle
